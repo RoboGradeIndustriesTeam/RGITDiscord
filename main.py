dc33f6e00fbe8e1701f6ac00a698076bd980f321
TheEnderOfficial <70067741+TheEnderOfficial@users.noreply.github.com>
2021-03-16 20:50:52 +0300

    Hello function

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import discord
-from discord.ext.commands import Bot
+import discord.ext.commands as commands
 from dotenv import load_dotenv
 load_dotenv()
 import os
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} запущен')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Хай, {ctx.message.author.mention}")
+
+bot.run(os.getenv('TOKEN'))
